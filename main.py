32faedb7030190e1eb4dfc235abf6bbc497b5685
Gulmira <gulmiraisakbaeva78@gmail.com>
2026-05-10 22:12:25 +0600

    lightweight auth

--- main.py
+++ main.py
@@ -1,64 +1,34 @@
-from fastapi import FastAPI, Depends
-from pydantic import BaseModel
-from sqlalchemy import select
+from http.client import responses
 
-from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
-from typing import Annotated
+from fastapi import FastAPI, HTTPException
+from authx import AuthX, AuthXConfig
+from fastapi.params import Depends
+from pydantic import BaseModel
+from starlette.responses import Response
+
 app = FastAPI()
 
-engine = create_async_engine('sqlite+aiosqlite:///books.db')
+config = AuthXConfig()
+config.JWT_SECRET_KEY = ''
+config.JWT_ACCESS_COOKIE_NAME = ''
+config.JWT_TOKEN_LOCATION = ["cookies"]
 
-new_session = async_sessionmaker(engine, expire_on_commit=False)
+security = AuthX(config=config)
 
-async def get_session():
-    async with new_session() as session:
-        yield session
+class UserLoginSchema(BaseModel):
+    username: str
+    password:str
 
-
-SessionDep = Annotated[AsyncSession, Depends(get_session)]
-
-
-class Base(DeclarativeBase):
-    pass
-
-class BookModel(Base):
-    __tablename__ = "books"
-
-    id: Mapped[int] = mapped_column(primary_key=True)
-    title: Mapped[str]
-    author: Mapped[str]
-
-@app.post("/setup_database")
-async def setup_database():
-    async with engine.begin() as conn:
-        await conn.run_sync(Base.metadata.drop_all)
-        await conn.run_sync(Base.metadata.create_all)
-    return {"ok": True}
+@app.post("/login")
+def login(creds: UserLoginSchema, response: Response):
+    if creds.username == "test" and creds.password == "test":
+        token = security.create_access_token(uid="12345")
+        response.set_cookie(config.JWT_ACCESS_COOKIE_NAME, token)
+        return {"access_token": token}
+    raise HTTPException(status_code=401, detail="пароль или имя не верное")
 
 
-class BookAddSchema(BaseModel):
-    title: str
-    author: str
 
-
-class BookSchema(BookAddSchema):
-    id: int
-
-
-@app.post("/books")
-async def add_book(data: BookAddSchema, session: SessionDep):
-    new_book = BookModel(
-        title=data.title,
-        author=data.author,
-    )
-    session.add(new_book)
-    await session.commit()
-    return {"ok": True}
-
-
-@app.get("/books")
-async def get_book(session: SessionDep):
-    query = select(BookModel)
-    result = await session.execute(query)
-    return result.scalars().all()
+@app.get("/protected", dependencies=[Depends(security.access_token_required)])
+def protected():
+    return {"data": "Mind Gigi"}
